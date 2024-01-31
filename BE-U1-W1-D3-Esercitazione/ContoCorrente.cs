badec1e0e510bef205b535830f5e2bfbc1ea3882
Jacopo Guelpa <jacopo.guelpa@gmail.com>
2024-01-31 16:16:44 +0100

    Aggiungere i file di progetto.

---
+++ BE-U1-W1-D3-Esercitazione/ContoCorrente.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_U1_W1_D3_Esercitazione
+{
+    internal class ContoCorrente
+    {
+        public string Titolare { get; set; }
+        public int NumeroConto { get; set; }
+        public double Saldo { get; set; }
+
+        public bool contoAperto = false;
+
+
+        public void AperturaConto(string titolare, int numeroConto, double saldo=1000.0)
+        {
+            if(!contoAperto)
+            {
+                Titolare = titolare;
+                NumeroConto = numeroConto;
+                Saldo = saldo;
+                contoAperto = true;
+
+                Console.WriteLine("Il conto di " + Titolare + ", numero: " + NumeroConto + " con un versamento iniziale di " + Saldo + " è stato aperto con successo!");
+            }
+            else
+            {
+                Console.WriteLine("Il conto è gia stato aperto.");
+            }
+
+            
+            /*Console.WriteLine("Vuoi aprire un nuovo conto? Scrivi si o no");
+           string risposta = Console.ReadLine();
+            if(risposta == "si") 
+            {
+                Console.WriteLine("Inserisci il numero del conto: ");
+             int numeroDelConto = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Inserisci il tuo nome e cognome: ");
+                string titolareDelConto = Console.ReadLine();
+
+                int saldoIniziale = 
+                    }*/
+
+        }
+
+        public void Versamento (double importo) 
+        { 
+            if(contoAperto)
+            {
+                Saldo += importo;
+                Console.WriteLine("Versamento di " + importo + " effettuato con successo!");
+            }
+            else 
+            { 
+                Console.WriteLine("Il conto non è aperto."); 
+            }
+        
+        }
+
+        public void Prelievo (double importo)
+        {
+            if (contoAperto && importo <= Saldo )
+            { 
+                Saldo -= importo;
+                Console.WriteLine("Prelievo di " + importo + " effettuato con successo!");
+            }
+            else 
+            { 
+                Console.WriteLine("Il conto non è aperto o fondi insufficienti");
+            }
+        }
+
+        public void VisualizzaSaldo ()
+        { 
+            Console.WriteLine("Il saldo disponibile è: " + Saldo);
+        }
+
+    }
+}
---
+++ BE-U1-W1-D3-Esercitazione/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_U1_W1_D3_Esercitazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContoCorrente contoUtente1 = new ContoCorrente();
+            contoUtente1.AperturaConto("Luca Rossi", 123456789);
+
+            contoUtente1.VisualizzaSaldo();
+            contoUtente1.Versamento(1500);
+            contoUtente1.VisualizzaSaldo();
+            contoUtente1.Prelievo(500);
+            contoUtente1.VisualizzaSaldo();
+
+
+            //----------------------------------------------------//
+
+
+            string[] vettore = { "Mario", "Luca", "Attila", "Beppe", "Giuda" };
+
+            Console.WriteLine("Inserisci il nome da cercare:");
+           
+            string nome = Console.ReadLine();
+            bool nomePresente = false;
+
+            for (int i = 0; i < vettore.Length; i++)
+            {
+                if (nome == vettore[i])
+                {
+                    nomePresente = true;
+                    break; 
+                }
+            }
+
+            if (nomePresente)
+            {
+                Console.WriteLine("Il nome è presente nella lista.");
+            }
+            else
+            {
+                Console.WriteLine("Il nome non è presente nella lista.");
+            }
+
+
+
+            //----------------------------------------------------//
+
+
+            int[] vettore2 = new int[3];
+
+            int x = vettore2[0];
+            int y = vettore2[1];
+            int z = vettore2[2];
+
+            x = 3; y= 5; z = 7;   
+
+            int somma = x + y + z;
+            int media = somma/vettore2.Length;
+
+            Console.WriteLine("La somma dei numeri è: " + somma);
+            Console.WriteLine("La media dei numeri è: " + media);
+
+            Console.ReadLine() ;
+        }
+    }
+}
